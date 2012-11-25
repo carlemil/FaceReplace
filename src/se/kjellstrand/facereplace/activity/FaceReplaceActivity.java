6e605f0e6187aa1c5ec515dfdf6558a14cf87fad
Carl-Emil Kjellstrand <erbsman@gmail.com>
2012-11-25 15:57:46 +0000

    image rotation corrected

--- src/se/kjellstrand/facereplace/activity/FaceReplaceActivity.java
+++ src/se/kjellstrand/facereplace/activity/FaceReplaceActivity.java
@@ -2,12 +2,15 @@
 package se.kjellstrand.facereplace.activity;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -66,14 +69,51 @@ public class FaceReplaceActivity extends Activity {
                 imageUri = mFileUri;
             }
 
-            bitmap = BitmapFactory.decodeFile(imageUri.getPath());
+            //bitmap = BitmapFactory.decodeFile(imageUri.getPath());
 
+            bitmap = getImageFromSDCard(imageUri.getPath());
+            
             Log.d("", "bitmap: " + bitmap);
 
             faceView.setBitmap(bitmap);
 
             faceView.findFaces();
         }
+    }
+    
+    private Bitmap getImageFromSDCard(String path){
+        try {
+            File f = new File(path);
+            ExifInterface exif = new ExifInterface(f.getPath());
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+            int angle = 0;
+
+            if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                angle = 90;
+            } 
+            else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                angle = 180;
+            } 
+            else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                angle = 270;
+            }
+
+            Matrix mat = new Matrix();
+            mat.postRotate(angle);
+
+            Bitmap bmp = BitmapFactory.decodeStream(new FileInputStream(f), null, null);
+            Bitmap correctBmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), mat, true);
+            
+            return correctBmp;
+        }
+        catch (IOException e) {
+            Log.w("TAG", "-- Error in setting image");
+        }   
+        catch(OutOfMemoryError oom) {
+            Log.w("TAG", "-- OOM Error in setting image");
+        }
+        return null;
     }
 
     private Uri getTempFileName()
--- src/se/kjellstrand/facereplace/view/FaceView.java
+++ src/se/kjellstrand/facereplace/view/FaceView.java
@@ -1,3 +1,4 @@
+
 package se.kjellstrand.facereplace.view;
 
 import android.content.Context;
@@ -13,18 +14,18 @@ import android.util.Log;
 import android.view.View;
 
 public class FaceView extends View {
-    private final String            TAG            = FaceView.class.getSimpleName();
+    private final String TAG = FaceView.class.getSimpleName();
 
-    private static final int        NUM_FACES      = 10;
-    private static final float      GOLDEN_RATIO   = 1.61803399f;
-    private static Bitmap           bitmap;
+    private static final int NUM_FACES = 10;
+    private static final float GOLDEN_RATIO = 1.61803399f;
+    private static Bitmap bitmap;
 
-    private FaceDetector            facesDetector;
-    private final FaceDetector.Face faces[]        = new FaceDetector.Face[NUM_FACES];
-    private FaceDetector.Face       face           = null;
+    private FaceDetector facesDetector;
+    private final FaceDetector.Face faces[] = new FaceDetector.Face[NUM_FACES];
+    private FaceDetector.Face face = null;
 
-    private final PointF            eyesMidPts[]   = new PointF[NUM_FACES];
-    private final float             eyesDistance[] = new float[NUM_FACES];
+    private final PointF eyesMidPts[] = new PointF[NUM_FACES];
+    private final float eyesDistance[] = new float[NUM_FACES];
 
     public FaceView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
@@ -45,18 +46,22 @@ public class FaceView extends View {
             Paint tmpPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
             tmpPaint.setColor(0xffffffff);
             canvas.drawBitmap(bitmap, null, new Rect(0, 0, getWidth(), getHeight()), tmpPaint);
-            for(int i = 0; i < eyesMidPts.length; i++) {
+            for (int i = 0; i < eyesMidPts.length; i++) {
                 if (eyesMidPts[i] != null) {
                     tmpPaint.setStrokeWidth(3);
-                    // canvas.drawCircle(eyesMidPts[i].x * xRatio, eyesMidPts[i].y * yRatio, eyesDistance[i] / 2, tmpPaint);
-                    // canvas.drawCircle(eyesMidPts[i].x * xRatio, eyesMidPts[i].y * yRatio, eyesDistance[i] / 6, tmpPaint);
+                    // canvas.drawCircle(eyesMidPts[i].x * xRatio,
+                    // eyesMidPts[i].y * yRatio, eyesDistance[i] / 2, tmpPaint);
+                    // canvas.drawCircle(eyesMidPts[i].x * xRatio,
+                    // eyesMidPts[i].y * yRatio, eyesDistance[i] / 6, tmpPaint);
                     Log.d(TAG, "draw circle");
-                    canvas.drawCircle(eyesMidPts[i].x * xRatio, eyesMidPts[i].y * yRatio, eyesDistance[i] / 2, tmpPaint);
+                    canvas.drawCircle(eyesMidPts[i].x * xRatio, eyesMidPts[i].y * yRatio,
+                            eyesDistance[i] / 2, tmpPaint);
                     RectF oval = new RectF();
                     float left = eyesMidPts[i].x * xRatio - eyesDistance[i] * xRatio;
                     float right = eyesMidPts[i].x * xRatio + eyesDistance[i] * xRatio;
                     float top = eyesMidPts[i].y * yRatio - eyesDistance[i] * GOLDEN_RATIO * yRatio;
-                    float bottom = eyesMidPts[i].y * yRatio + eyesDistance[i] * GOLDEN_RATIO * yRatio;
+                    float bottom = eyesMidPts[i].y * yRatio + eyesDistance[i] * GOLDEN_RATIO
+                            * yRatio;
 
                     Log.d(TAG, "(eyesDistance[i] / 2) * xRatio: " + (eyesDistance[i] / 2) * xRatio);
 
@@ -74,23 +79,29 @@ public class FaceView extends View {
         int picWidth = bitmap.getWidth();
         int picHeight = bitmap.getHeight();
 
-        facesDetector = new FaceDetector(picWidth, picHeight, NUM_FACES);
-        facesDetector.findFaces(bitmap, faces);
+        if (bitmap != null) {
 
-        for(int i = 0; i < faces.length; i++) {
-            face = faces[i];
-            if (face != null) {
-                PointF eyesMP = new PointF();
-                face.getMidPoint(eyesMP);
-                eyesDistance[i] = face.eyesDistance();
-                eyesMidPts[i] = eyesMP;
+            facesDetector = new FaceDetector(picWidth, picHeight, NUM_FACES);
+            facesDetector.findFaces(bitmap, faces);
 
-                Log.d("Face",
-                        i + " " + face.confidence() + " " + face.eyesDistance() + " " + "Pose: ("
-                                + face.pose(FaceDetector.Face.EULER_X) + "," + face.pose(FaceDetector.Face.EULER_Y) + ","
-                                + face.pose(FaceDetector.Face.EULER_Z) + ")" + "Eyes Midpoint: (" + eyesMidPts[i].x + ","
-                                + eyesMidPts[i].y + ")");
+            for (int i = 0; i < faces.length; i++) {
+                face = faces[i];
+                if (face != null) {
+                    PointF eyesMP = new PointF();
+                    face.getMidPoint(eyesMP);
+                    eyesDistance[i] = face.eyesDistance();
+                    eyesMidPts[i] = eyesMP;
 
+                    Log.d("Face",
+                            i + " " + face.confidence() + " " + face.eyesDistance() + " "
+                                    + "Pose: ("
+                                    + face.pose(FaceDetector.Face.EULER_X) + ","
+                                    + face.pose(FaceDetector.Face.EULER_Y) + ","
+                                    + face.pose(FaceDetector.Face.EULER_Z) + ")"
+                                    + "Eyes Midpoint: (" + eyesMidPts[i].x + ","
+                                    + eyesMidPts[i].y + ")");
+
+                }
             }
         }
         Log.d(TAG, "Finding faces took " + (System.currentTimeMillis() - startTime) + " ms.");
